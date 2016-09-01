604784e4f857583e5c164ebb1009c5246872810e
Dmitry Martynov <domartynov@gmail.com>
2016-09-01 09:32:54 -0400

    Bump version to 0.1.1

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,4 +1,4 @@
-#### 0.1.0 - 08/31/2016
+#### 0.1.1 - 09/01/2016
 * Setup project
 * basic updater
 * publisher publish command
--- src/Updater.Publish/AssemblyInfo.fs
+++ src/Updater.Publish/AssemblyInfo.fs
@@ -5,11 +5,11 @@ open System.Reflection
 [<assembly: AssemblyProductAttribute("Updater")>]
 [<assembly: AssemblyCompanyAttribute("Dzmitry Martynau")>]
 [<assembly: AssemblyDescriptionAttribute("Deploy, update and publish tools for Windows client applications.")>]
-[<assembly: AssemblyVersionAttribute("0.1.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.1.0")>]
-[<assembly: AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly: AssemblyVersionAttribute("0.1.1")>]
+[<assembly: AssemblyFileVersionAttribute("0.1.1")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.1.1")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1.0"
-    let [<Literal>] InformationalVersion = "0.1.0"
+    let [<Literal>] Version = "0.1.1"
+    let [<Literal>] InformationalVersion = "0.1.1"
